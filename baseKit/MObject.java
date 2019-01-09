0964a93e05db038ae8b3a32b3381160462e326b9
MH2012 <havnetrold2002@yahoo.dk>
2019-01-09 16:07:00 +0100

    Added new files

---
+++ baseKit/MObject.java
@@ -0,0 +1,51 @@
+package baseKit;
+
+
+public class MObject  {
+	
+	public MObject() {
+		
+		id++;
+	}
+	
+	protected void print(String txt)
+	{
+		System.out.println(txt);
+	}
+	
+	protected void print(String txt, printMode mode)
+	{
+		if (mode == printMode.lineBreak)
+			System.out.println(txt);
+		else
+			System.out.print(txt);
+	}
+	
+	/*
+	 * Meta information related
+	 */
+	
+	public String ClassName()
+	{
+		print(getClass().toString(),printMode.lineBreak);
+		return getClass().toString();
+	}
+	
+	public void setObjectName(String name)
+	{
+		objectName = name;
+	}
+	
+	public String ObjectName()
+	{
+		return objectName;
+	}
+	
+	public enum printMode {lineBreak,noLinebreak};
+	private String objectName = String.format("Object(%d)", id);
+	private static int id = 1;
+	
+	// Trash methods. Just ignore.
+
+	
+}
---
+++ guiExample/FoodObject.java
@@ -0,0 +1,11 @@
+package guiExample;
+
+import baseKit.MWidget;
+
+public class FoodObject extends MWidget {
+
+	public FoodObject(MainWindow parent) {
+		super(parent);
+	}
+
+}
